78ace43e42f02e0847b84ea1718b985f7a43095e
Danielle <danineubauerr@gmail.com>
2019-12-24 03:10:16 +0200

    adds text

---
+++ Typewriter.js
@@ -0,0 +1,40 @@
+var aText = new Array(
+    "Hey you!", 
+    "I'm Danielle Neubauer.", // add your text
+    "A full-stack developer.", // add your text
+    "Together we can:",
+    "architect, engineer & revive", // add your text
+    "your greatest dreams & visions", // add your text
+    );
+    var iSpeed = 100; 
+    var iIndex = 0; 
+    var iArrLength = aText[0].length; 
+    var iScrollAt = 20; 
+     
+    var iTextPos = 0; 
+    var sContents = ''; 
+    var iRow; 
+     
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     var destination = document.getElementById("typedtext");
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 500);
+      }
+     } else {
+      setTimeout("typewriter()", iSpeed);
+     }
+    }
+
+typewriter();

e94f926d05d4c5d3c4410ffa7f5ac50d76c55c2e
Danielle <danineubauerr@gmail.com>
2019-12-30 17:28:14 +0200

    adds right menu

--- Typewriter.js
+++ Typewriter.js
@@ -52,4 +52,23 @@ setTimeout(button, 6300);
 
 
 
+const menu = document.querySelector('#toggle');  
+const menuItems = document.querySelector('#overlay');  
+const menuContainer = document.querySelector('.menu-container');  
+const menuIcon = document.querySelector('i');  
+
+function toggleMenu(e) {
+    menuItems.classList.toggle('open');
+    menuContainer.classList.toggle('full-menu');
+    menuIcon.classList.toggle('fa-bars');
+    menuIcon.classList.add('fa-times');
+    e.preventDefault();
+}
+
+menu.addEventListener('click', toggleMenu, false);
+
+
+
+
+
 

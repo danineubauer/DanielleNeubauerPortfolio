245585094bf4440b12d6c9ff2300410cb3d64378
Danielle <danineubauerr@gmail.com>
2020-01-02 15:44:09 +0200

    saves changes

--- mtypewriter.js
+++ mtypewriter.js
@@ -72,3 +72,177 @@ $(menu).click(function(){
 
 
 
+
+//-------------------------------------------Navigation bar and layout----------------------------------//
+
+class StickyNavigation {
+	
+	constructor() {
+		this.currentId = null;
+		this.currentTab = null;
+		this.tabContainerHeight = 70;
+		let self = this;
+		$('.et-hero-tab').click(function() { 
+			self.onTabClick(event, $(this)); 
+		});
+		$(window).scroll(() => { this.onScroll(); });
+		$(window).resize(() => { this.onResize(); });
+	}
+	
+	onTabClick(event, element) {
+		event.preventDefault();
+		let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
+		$('html, body').animate({ scrollTop: scrollTop }, 600);
+	}
+	
+	onScroll() {
+		this.checkTabContainerPosition();
+    this.findCurrentTabSelector();
+	}
+	
+	onResize() {
+		if(this.currentId) {
+			this.setSliderCss();
+		}
+	}
+	
+	checkTabContainerPosition() {
+		let offset = $('.et-hero-tabs').offset().top + $('.et-hero-tabs').height() - this.tabContainerHeight;
+		if($(window).scrollTop() > offset) {
+			$('.et-hero-tabs-container').addClass('et-hero-tabs-container--top');
+		} 
+		else {
+			$('.et-hero-tabs-container').removeClass('et-hero-tabs-container--top');
+		}
+	}
+	
+	findCurrentTabSelector(element) {
+		let newCurrentId;
+		let newCurrentTab;
+		let self = this;
+		$('.et-hero-tab').each(function() {
+			let id = $(this).attr('href');
+			let offsetTop = $(id).offset().top - self.tabContainerHeight;
+			let offsetBottom = $(id).offset().top + $(id).height() - self.tabContainerHeight;
+			if($(window).scrollTop() > offsetTop && $(window).scrollTop() < offsetBottom) {
+				newCurrentId = id;
+				newCurrentTab = $(this);
+			}
+		});
+		if(this.currentId != newCurrentId || this.currentId === null) {
+			this.currentId = newCurrentId;
+			this.currentTab = newCurrentTab;
+			this.setSliderCss();
+		}
+	}
+	
+	setSliderCss() {
+		let width = 0;
+		let left = 0;
+		if(this.currentTab) {
+			width = this.currentTab.css('width');
+			left = this.currentTab.offset().left;
+		}
+		$('.et-hero-tab-slider').css('width', width);
+		$('.et-hero-tab-slider').css('left', left);
+	}
+	
+}
+
+new StickyNavigation();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var formContainer = $('#form-container');
+
+bindFormClick();
+//Opening the form
+function bindFormClick(){
+  $(formContainer).on('click', function(e) {
+    e.preventDefault();
+    toggleForm();
+    //Ensure container doesn't togleForm when open
+    $(this).off();
+  });
+}
+
+//Closing the form
+$('#form-close, .form-overlay').click(function(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  toggleForm();
+  bindFormClick();
+});
+
+function toggleForm(){
+  $(formContainer).toggleClass('expand');
+  $(formContainer).children().toggleClass('expand');
+  $('body').toggleClass('show-form-overlay');
+  $('.form-submitted').removeClass('form-submitted');
+}
+
+//Form validation
+$('form').submit(function() {
+  var form = $(this);
+  form.find('.form-error').removeClass('form-error');
+  var formError = false;
+  
+  form.find('.input').each(function() {
+    if ($(this).val() == '') {
+      $(this).addClass('form-error');
+      $(this).select();
+      formError = true;
+      return false;
+    }
+    else if ($(this).hasClass('email') && !isValidEmail($(this).val())) {
+      $(this).addClass('form-error');
+      $(this).select();
+      formError = true;
+      return false;
+    }
+  });
+  
+  if (!formError) {
+    $('body').addClass('form-submitted');
+    $('#form-head').addClass('form-submitted'); 
+    setTimeout(function(){
+      $(form).trigger("reset");
+    }, 1000);
+  }
+  return false;
+});
+
+function isValidEmail(email) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(email);
+};
+
+social("twitter/joeharry__", "codepen/woodwork",
+"disco");

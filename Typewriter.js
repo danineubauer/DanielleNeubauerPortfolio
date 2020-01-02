abb498db087efa62ab29947610ca7b4233e48e97
Danielle <danineubauerr@gmail.com>
2020-01-02 13:10:24 +0200

    adds main page

--- Typewriter.js
+++ Typewriter.js
@@ -2,9 +2,9 @@
 
 
 var aText = new Array(
-    "I'm a full-stack developer based in Berkeley, California.", // add your text
-    "Together we can architect, engineer & revive", // add your text
-    "your greatest dreams & visions.", // add your text
+    "I'm a full-stack developer based in Berkeley, California.",
+    "Together we can plan, engineer and design,", 
+    "your greatest dreams & visions.",
     );
     var iSpeed = 30; 
     var iIndex = 0; 
@@ -51,7 +51,7 @@ setTimeout(button, 6300);
 
 
 
-//MENU SIDE 
+//MENU ON THE RIGHT SIDE 
 const menu = document.querySelector('#toggle');  
 const menuItems = document.querySelector('#overlay');  
 const menuContainer = document.querySelector('.menu-container');  
@@ -66,15 +66,259 @@ function toggleMenu(e) {
     e.preventDefault();
 }
 
-function homePageMenu() { 
-    homePage.style.visibility = 'hidden';
-}
-
 menu.addEventListener('click', toggleMenu, false);
 
-menu.addEventListener('click', homePageMenu);
+//----------Toggles between homepage and menu-------------
+$(menu).click(function(){ 
+    $('#homePageElement').toggle();
+    $('.icon-bar').toggle(); 
+    $('.arrow').toggle();
+})
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // State
+// const state = {
+// 	animations: ['fade', 'slide', 'slideUp', 'zoom', 'flipX', 'flipY'],
+// 	view: 'slide'
+// }
+
+// // Controls
+// const controls = Vue.component('controls', {
+//   template: '#controls',
+// 	data: state,
+// 	methods: {
+// 		setView(animation) {
+// 			state.view = animation
+// 		}
+// 	}
+// })
+
+// // Transitions
+// const fade = Vue.component('fade', {
+//   template: '#page',
+// 	methods: {
+// 		enter(el, done) {
+// 			TweenMax.fromTo(el, 1, {
+// 				autoAlpha: 0,
+// 				scale: 1.5,
+// 			}, {
+// 				autoAlpha: 1,
+// 				scale: 1,
+// 				transformOrigin: '50% 50%',
+// 				ease: Power4.easeOut,
+// 				onComplete: done
+// 			});
+// 		},
+// 		leave(el, done) {
+// 			TweenMax.fromTo(el, 1, {
+// 				autoAlpha: 1,
+// 				scale: 1,
+// 			}, {
+// 				autoAlpha: 0,
+// 				scale: 0.8,
+// 				ease: Power4.easeOut,
+// 				onComplete: done
+// 			});
+// 		}
+// 	}
+// })
+
+// const slide = Vue.component('slide', {
+//   template: '#page',
+// 	methods: {
+// 		enter(el, done) {
+// 			const tl = new TimelineMax({
+// 				onComplete: done
+// 			})
+			
+// 			tl.set(el, {
+// 				x: window.innerWidth * 1.5,
+// 				scale: 0.8,
+// 				transformOrigin: '50% 50%'
+// 			})
+			
+// 			tl.to(el, 0.5, {
+// 				x: 0,
+// 				ease: Power4.easeOut
+// 			});
+			
+// 			tl.to(el, 1, {
+// 				scale: 1,
+// 				ease: Power4.easeOut
+// 			});
+// 		},
+// 		leave(el, done) {
+// 			TweenMax.fromTo(el, 1, {
+// 				autoAlpha: 1
+// 			}, {
+// 				autoAlpha: 0,
+// 				ease: Power4.easeOut,
+// 				onComplete: done
+// 			});
+// 		}	
+// 	}
+// })
+
+// const slideUp = Vue.component('slideUp', {
+//   template: '#page',
+// 	methods: {
+// 		enter(el, done) {
+// 			const tl = new TimelineMax({
+// 				onComplete: done
+// 			})
+			
+// 			tl.set(el, {
+// 				y: window.innerWidth * 1.5,
+// 				scale: 0.8,
+// 				transformOrigin: '50% 50%'
+// 			})
+			
+// 			tl.to(el, 0.5, {
+// 				y: 0,
+// 				ease: Power4.easeOut
+// 			});
+			
+// 			tl.to(el, 1, {
+// 				scale: 1,
+// 				ease: Power4.easeOut
+// 			});
+// 		},
+// 		leave(el, done) {
+// 			TweenMax.to(el, 1, {
+// 				y: window.innerHeight * -1.5,
+// 				ease: Power4.easeOut,
+// 				onComplete: done
+// 			});
+// 		}	
+// 	}
+// })
+
+// const zoom = Vue.component('zoom', {
+//   template: '#page',
+// 	methods: {
+// 		enter(el, done) {
+// 			const tl = new TimelineMax({
+// 				onComplete: done
+// 			})
+			
+// 			tl.set(el, {
+// 				autoAlpha: 0,
+// 				scale: 2,
+// 				transformOrigin: '50% 50%'
+// 			})
+			
+// 			tl.to(el, 1, {
+// 				autoAlpha: 1,
+// 				scale: 1,
+// 				ease: Power4.easeOut
+// 			})
+// 		},
+// 		leave(el, done) {
+// 			TweenMax.to(el, 1, {
+// 				scale: 0,
+// 				ease: Power4.easeOut,
+// 				onComplete: done
+// 			});
+// 		}	
+// 	}
+// })
+
+// const flipX = Vue.component('flipX', {
+//   template: '#page',
+// 	methods: {
+// 		enter(el, done) {
+// 			const tl = new TimelineMax({
+// 				onComplete: done
+// 			})
+			
+// 			tl.set(el, {
+// 				autoAlpha: 0,
+// 				rotationX: 90,
+// 				transformOrigin: '50% 50%'
+// 			})
+			
+// 			tl.to(el, 1, {
+// 				autoAlpha: 1,
+// 				rotationX: 0,
+// 				ease: Power4.easeOut
+// 			})
+// 		},
+// 		leave(el, done) {
+// 			TweenMax.to(el, 1, {
+// 				scale: 0,
+// 				ease: Power4.easeOut,
+// 				onComplete: done
+// 			});
+// 		}	
+// 	}
+// })
+
+// const flipY = Vue.component('flipY', {
+//   template: '#page',
+// 	methods: {
+// 		enter(el, done) {
+// 			const tl = new TimelineMax({
+// 				onComplete: done
+// 			})
+			
+// 			tl.set(el, {
+// 				autoAlpha: 0,
+// 				rotationY: 90,
+// 				transformOrigin: '50% 50%'
+// 			})
+			
+// 			tl.to(el, 1, {
+// 				autoAlpha: 1,
+// 				rotationY: 0,
+// 				ease: Power4.easeOut
+// 			})
+// 		},
+// 		leave(el, done) {
+// 			TweenMax.to(el, 1, {
+// 				scale: 0,
+// 				ease: Power4.easeOut,
+// 				onComplete: done
+// 			});
+// 		}	
+// 	}
+// })
+
+// // App
+// const app = new Vue({
+//   el: '#app',
+//   data() {
+// 		return state
+// 	}
+// })
 
 

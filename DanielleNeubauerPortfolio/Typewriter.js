4c278c8df04ad5c8aef61ac6ea1fe58fb2565369
Danielle <danineubauerr@gmail.com>
2020-02-26 15:29:15 -0800

    adds about me

--- DanielleNeubauerPortfolio/Typewriter.js
+++ DanielleNeubauerPortfolio/Typewriter.js
@@ -1,19 +1,19 @@
 // Page flip 
-// var siteW = $(window).width();
-// var siteH = $(window).height();
+var siteW = $(window).width();
+var siteH = $(window).height();
 
-// $("p").css({ lineHeight: siteH + 'px' }); // dirty dirty
+$("p").css({ lineHeight: siteH + 'px' }); // dirty dirty
 
-// TweenMax.set(".site", { perspective: 5000 });
-// TweenMax.set(".container", { transformStyle: "preserve-3d",  transformOrigin: '-0% 50%' });
-// TweenMax.set("#new-content", { rotationY: 90, z: -siteW/2, x: siteW/2 });
+TweenMax.set(".site", { perspective: 5000 });
+TweenMax.set(".container", { transformStyle: "preserve-3d",  transformOrigin: '-0% 50%' });
+TweenMax.set("#new-content", { rotationY: 90, z: -siteW/2, x: siteW/2 });
 
-// var tlFlip = new TimelineMax({ yoyo: true, repeat: -1, delay: 1.5, repeatDelay: 2 });
+var tlFlip = new TimelineMax({ yoyo: true, repeat: -1, delay: 1.5, repeatDelay: 2 });
 
-// tlFlip
-// .to('.site', .5, { scale: 0.8, ease: Power2.easeInOut }, "start")
-// .to('.container', .4, { rotationY: -90, z: -siteW, ease: Power2.easeInOut }, "start+=0.7")
-// .to('.site', .5, { scale: 1, ease: Power2.easeInOut }, "start+=1.2");
+tlFlip
+.to('.site', .5, { scale: 0.8, ease: Power2.easeInOut }, "start")
+.to('.container', .4, { rotationY: -90, z: -siteW, ease: Power2.easeInOut }, "start+=0.7")
+.to('.site', .5, { scale: 1, ease: Power2.easeInOut }, "start+=1.2");
 
 
 
@@ -343,87 +343,5 @@ $('#button').click(function() {
 // 		return state
 // 	}
 // })
-
-
-
-
-//-------------------------------------------Navigation bar and layout----------------------------------//
-
-class StickyNavigation {
-	
-	constructor() {
-		this.currentId = null;
-		this.currentTab = null;
-		this.tabContainerHeight = 70;
-		let self = this;
-		$('.et-hero-tab').click(function() { 
-			self.onTabClick(event, $(this)); 
-		});
-		$(window).scroll(() => { this.onScroll(); });
-		$(window).resize(() => { this.onResize(); });
-	}
-	
-	onTabClick(event, element) {
-		event.preventDefault();
-		let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
-		$('html, body').animate({ scrollTop: scrollTop }, 600);
-	}
-	
-	onScroll() {
-		this.checkTabContainerPosition();
-    this.findCurrentTabSelector();
-	}
-	
-	onResize() {
-		if(this.currentId) {
-			this.setSliderCss();
-		}
-	}
-	
-	checkTabContainerPosition() {
-		let offset = $('.et-hero-tabs').offset().top + $('.et-hero-tabs').height() - this.tabContainerHeight;
-		if($(window).scrollTop() > offset) {
-			$('.et-hero-tabs-container').addClass('et-hero-tabs-container--top');
-		} 
-		else {
-			$('.et-hero-tabs-container').removeClass('et-hero-tabs-container--top');
-		}
-	}
-	
-	findCurrentTabSelector(element) {
-		let newCurrentId;
-		let newCurrentTab;
-		let self = this;
-		$('.et-hero-tab').each(function() {
-			let id = $(this).attr('href');
-			let offsetTop = $(id).offset().top - self.tabContainerHeight;
-			let offsetBottom = $(id).offset().top + $(id).height() - self.tabContainerHeight;
-			if($(window).scrollTop() > offsetTop && $(window).scrollTop() < offsetBottom) {
-				newCurrentId = id;
-				newCurrentTab = $(this);
-			}
-		});
-		if(this.currentId != newCurrentId || this.currentId === null) {
-			this.currentId = newCurrentId;
-			this.currentTab = newCurrentTab;
-			this.setSliderCss();
-		}
-	}
-	
-	setSliderCss() {
-		let width = 0;
-		let left = 0;
-		if(this.currentTab) {
-			width = this.currentTab.css('width');
-			left = this.currentTab.offset().left;
-		}
-		$('.et-hero-tab-slider').css('width', width);
-		$('.et-hero-tab-slider').css('left', left);
-	}
-	
-}
-
-new StickyNavigation();
-
 
 
